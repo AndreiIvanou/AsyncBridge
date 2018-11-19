5040edbf1128c4743273910e7f72ed467354bbec
Andrei Ivanou <Andrei_Ivanou@epam.com>
2018-11-19 17:30:25 +0100

    Bridge and Worker are connected via Redis

--- AsyncBridge/Bridge/Controllers/JobDispatcherController.cs
+++ AsyncBridge/Bridge/Controllers/JobDispatcherController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Bridge.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
 using StackExchange.Redis;
 
 namespace Bridge.Controllers
@@ -14,26 +16,64 @@ namespace Bridge.Controllers
     public class JobDispatcherController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<string> Get(string key)
+        public ActionResult<string> Get(string jobId)
         {
             using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost"))
             {
                 IDatabase db = redis.GetDatabase();
 
-                string result = db.StringGet(key);
+                RedisValue jobResult = db.StringGet(jobId);
 
-                return Ok(result);
+                if(jobResult == RedisValue.Null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(jobResult);
             }
         }
 
         [HttpPost]
-        public void Post([FromBody] SubmitJobRequest request)
+        public ActionResult<string> Post([FromBody] SubmitJobRequest request)
+        {
+            string jobId = SendToRabbitMq(request.Name, request.JobParameters);
+
+            return Accepted(jobId);
+        }
+
+        private static string SendToRabbitMq(string jobName, string jsonRequest)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.CorrelationId = Guid.NewGuid().ToString();
+
+                channel.ExchangeDeclare(exchange: "jobs", type: "topic");
+
+                var routingKey = jobName;
+                var message = jsonRequest;
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "jobs",
+                                     routingKey: routingKey,
+                                     basicProperties: properties,
+                                     body: body);
+
+                WriteToRedis(properties.CorrelationId, "In progress");
+
+                return properties.CorrelationId;
+            }
+        }
+
+        private static void WriteToRedis(string key, string value)
         {
             using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost"))
             {
                 IDatabase db = redis.GetDatabase();
 
-                db.StringSet(request.Name, request.JobParameters);
+                db.StringSet(key, value);
             }
         }
 
--- AsyncBridge/Worker/Program.cs
+++ AsyncBridge/Worker/Program.cs
@@ -2,56 +2,64 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
+using Newtonsoft.Json;
+using StackExchange.Redis;
 
 namespace Worker
 {
     class Program
     {
-        public static void Main()
+        class Parameters
+        {
+            public int Number { get; set; }
+        }
+
+        public static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "rpc_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
-                channel.BasicQos(0, 1, false);
-                var consumer = new EventingBasicConsumer(channel);
-                channel.BasicConsume(queue: "rpc_queue", autoAck: false, consumer: consumer);
-                Console.WriteLine(" [x] Awaiting RPC requests");
+                channel.ExchangeDeclare(exchange: "jobs", type: "topic");
+                var queueName = channel.QueueDeclare(durable: true).QueueName;
 
+
+                channel.QueueBind(queue: queueName, 
+                                  exchange: "jobs",
+                                  routingKey: "fibonacci");
+
+                var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
                 {
-                    string response = null;
-
                     var body = ea.Body;
-                    var props = ea.BasicProperties;
-                    var replyProps = channel.CreateBasicProperties();
-                    replyProps.CorrelationId = props.CorrelationId;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    var jobId = ea.BasicProperties.CorrelationId;
 
-                    try
+                    Console.WriteLine("Received message: {0}", message);
+                    Console.WriteLine("Correlation Id: {0}", jobId);
+
+                    Parameters parameters = JsonConvert.DeserializeObject<Parameters>(message);
+
+                    int result = Fib(parameters.Number);
+                    Console.WriteLine("Fibonacci: {0}", result);
+
+                    using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost"))
                     {
-                        var message = Encoding.UTF8.GetString(body);
-                        int n = int.Parse(message);
-                        Console.WriteLine(" [.] fib({0})", message);
-                        response = Fib(n).ToString();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(" [.] " + e.Message);
-                        response = "";
-                    }
-                    finally
-                    {
-                        var responseBytes = Encoding.UTF8.GetBytes(response);
-                        channel.BasicPublish(exchange: "", routingKey: props.ReplyTo, basicProperties: replyProps, body: responseBytes);
-                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                        IDatabase db = redis.GetDatabase();
+
+                        db.StringSet(jobId, result);
                     }
                 };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
 
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
             }
         }
+
         private static int Fib(int n)
         {
             if (n == 0 || n == 1)
